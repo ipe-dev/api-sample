b7c1eec7cbed974b97c92b7e8b84e9317fe874c6
ipe <izaki.yb1998@gmail.com>
2025-01-03 08:02:09 +0900

    ルーティング設計

---
+++ app/Http/Controllers/Todos/todoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Todos;
+
+class todoController extends Controller
+{
+    public function create() 
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,16 @@
 <?php
 
+use App\Http\Controllers¥todos\todoController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/users')->group(function() {
+    Route::post('/login', [UserController::class, 'login']);
+});
+
+Route::prefix('/todos')->group(function () {
+   Route::get('/', [todoController::class, 'index']); 
+   Route::post('/create', [todoController::class, 'create']);
+   Route::get('/{id}', [todoController::class, 'show']);
+   Route::delete('/{id}', [todoController::class, 'delete']);
+   Route::put('/{id}', [todoController::class, 'update']);
+});
