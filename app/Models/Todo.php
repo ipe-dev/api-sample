533e1a5e025c1e0632dc44367348486c4e27b7a3
ipe <izaki.yb1998@gmail.com>
2025-01-05 16:32:47 +0900

    propertyを追加

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -11,6 +11,19 @@ use OpenApi\Annotations as OA;
  *          description="Todo model",
  *          type="object",
  *          required={"id, name"}
+ *          @OA\Property(
+ *            property="id",
+ *            type="integer",
+ *            format="int64",
+ *            description="ID"
+ *            example=1
+ *            ),
+ *            @OA\Property(
+ *              property="name",
+ *              type="string",
+ *              description="Todo name",
+ *              example="Buy milk"
+ *            ),
  *      )
  */
 class Todo extends Model
