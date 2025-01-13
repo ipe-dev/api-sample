4660d4fb09bf97a9043b1deae17dab39346d4513
ipe <izaki.yb1998@gmail.com>
2025-01-14 05:58:53 +0900

    lint

--- app/Http/Controllers/Todos/todoListController.php
+++ app/Http/Controllers/Todos/todoListController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Todos;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use OpenApi\Annotations as OA;
 
 final class todoListController extends Controller
@@ -31,17 +32,20 @@ final class todoListController extends Controller
      *           property="message",
      *           type="string",
      *           example="Internal Server Error",
-     *           ),
-     *        ),
-     *             ),
+     *         ),
+     *       ),
+     *     ),
      * )
      */
-    public function __invoke() {
+    public function __invoke()
+    {
         try {
             $todos = \App\Models\Todo::all();
+
             return response()->json($todos);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             Log::error("Error: {$e->getMessage()}");
+
             return response()->json(['message' => 'Internal Server Error'], 500);
         }
     }
