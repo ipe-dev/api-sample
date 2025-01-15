a8039aeab15c69c0014262325c58fbe179e439e3
ipe <izaki.yb1998@gmail.com>
2025-01-16 06:56:14 +0900

    ファイル名修正

---
+++ app/Http/Controllers/Todos/TodoCreateController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Todos;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TodoCreateRequest;
+use OpenApi\Annotations as OA;
+
+final class todoCreateController extends Controller
+{
+    /**
+     * @OA\Post(
+     *      path="/todos/",
+     *      operationId="Create Todo",
+     *      tags={"Tags"},
+     *      summary="Todoを作成する",
+     *      description="Todoとなる文字列を受け取り、Todoを作成する",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/TodoCreateRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *       ),
+     *     )
+     */
+    public function __invoke(TodoCreateRequest $request) {}
+}
