c84323fa8046c08f184cbd90eb8738a86a5555dd
ipe <izaki.yb1998@gmail.com>
2025-01-16 06:56:39 +0900

    UserLoginController作成

---
+++ app/Http/Controllers/Users/UserLoginController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Users;
+
+use App\Http\Controllers\Controller;
+
+final class UserLoginController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,21 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Todos\todoListController;
+use App\Http\Controllers\Todos\todoCreateController;
+use App\Http\Controllers\Todos\TodoDeleteController;
+use App\Http\Controllers\Todos\TodoDetailController;
+use App\Http\Controllers\Todos\TodoListController;
+use App\Http\Controllers\Users\UserLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/users')->group(function () {
-    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/login', UserLoginController::class);
 });
 
 Route::prefix('/todos')->group(function () {
-    Route::get('/', [todoListController::class, 'index']);
-    Route::post('/', [todoCreateController::class, 'create']);
-    Route::get('/{id}', [todoDetailController::class, 'show']);
-    Route::delete('/{id}', [todoDeleteController::class, 'delete']);
-    Route::put('/{id}', [todoUpdateController::class, 'update']);
+    Route::get('/', TodoListController::class);
+    Route::post('/', TodoCreateController::class);
+    Route::get('/{id}', TodoDetailController::class);
+    Route::delete('/{id}', TodoDeleteController::class);
+    Route::put('/{id}', TodoUpdateController::class);
 });
